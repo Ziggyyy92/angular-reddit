61b2b027d7293627523920f3c8ac969e65f81ee2
Ziggyyy92 <igor.stajic92@gmail.com>
2023-08-06 14:36:00 +0200

    Update za Angular-Reddit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Article } from './article/article.model'; //dodata je ova linija
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  articles: Article[]; //osobina komponente
+
+  constructor() {
+    this.articles = [
+      new Article('IT255 - Veb sistemi 1', 'http://www.metropolitan.ac.rs', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+}
